9d150d5cfbb154f22ffaf25ad4130f6f3ca80a7b
Josh <reimer.josh.2006@gmail.com>
2024-12-22 16:15:00 -0400

    switching from dht11 to dht22

--- esp8266_temp_server/esp8266_temp_server.ino
+++ esp8266_temp_server/esp8266_temp_server.ino
@@ -62,8 +62,8 @@ byte daysavetime = 0;
 FSInfo fs_info;
 
 // Uncomment the type of sensor in use:
-#define DHTTYPE DHT11  // DHT 11
-//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE DHT11  // DHT 11
+#define DHTTYPE    DHT22     // DHT 22 (AM2302)
 //#define DHTTYPE    DHT21     // DHT 21 (AM2301)
 
 DHT dht(DHTPIN, DHTTYPE);
